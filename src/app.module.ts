c32eb5c23d26ff44b9984ac89a27f6eafd030d94
Maísa Lima <maisafernanda@hotmail.com>
2023-08-05 10:32:52 -0300

    refactor: cutomer_crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
+import { PrismaService } from './services/prisma-service';
+import { CustomerController } from 'src/controllers/customer-controller';
 import { Module } from '@nestjs/common';
+import { CustomerService } from './services/customer-service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [CustomerController],
+  providers: [PrismaService, CustomerService],
 })
 export class AppModule {}
--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,6 +1,12 @@
 const MESSAGE = {
-  SUCCESS: {},
-  ERROR: {},
+  SUCCESS: {
+    CUSTOMER_CREATED: 'Customer created successfully',
+    CUSTOMER_FOUND: 'Customer found',
+  },
+  ERROR: {
+    CUSTOMER_ALREADY: 'E-mail already registered',
+    CUSTOMER_NOT_FOUND: 'Customer not found',
+  },
 };
 
 export default MESSAGE;

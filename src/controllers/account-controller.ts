d4fba10d9bd43c66e4916f5df8c26b81956da543
Maísa Lima <maisafernanda@hotmail.com>
2023-08-05 13:37:08 -0300

    refactor: account_crud

--- src/controllers/account-controller.ts
+++ src/controllers/account-controller.ts
@@ -1,75 +1,63 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  HttpStatus,
-  Post,
-  Get,
-  Param,
-  Put,
-} from '@nestjs/common';
+import { CustomerService } from './../services/customer-service';
+import { Body, Controller, Post, Get, Param, Put } from '@nestjs/common';
 import { AccountService } from '../services/account-service';
 import { Account } from '@prisma/client';
 
 @Controller('account')
 export class AccountController {
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly customerService: CustomerService,
+  ) {}
+
   @Post()
-  async createAccount(@Body() account: Account) {
-    const data = await this.accountService.createAccount(account);
-    if (data.name === 'error') {
-      throw new BadRequestException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: data.message,
-      });
-    }
-    return {
-      statusCode: HttpStatus.OK,
-      message: data.message,
-    };
+  async createAccount(
+    @Body()
+    accountData: {
+      customer_id: number;
+      agency: number;
+      balance: number;
+      account_status: number;
+    },
+  ): Promise<Account> {
+    return this.accountService.createAccount(accountData);
   }
 
-  @Get('id')
-  async findOne(@Param('id') id: number) {
-    const data = await this.accountService.findOne(id);
-    if (data.name === 'error') {
-      throw new BadRequestException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: data.message,
-      });
-    }
-    return {
-      statusCode: HttpStatus.OK,
-      message: data.message,
-    };
+  @Get(':account_number')
+  async getAccount(@Param('account_number') account: string) {
+    return this.accountService.getAccount({
+      account_number: Number(account),
+    });
   }
 
-  @Get('/account/:account')
-  async findByAccount(@Param('account') account: number) {
-    const data = await this.accountService.findByAccount(account);
-    if (data.name === 'error') {
-      throw new BadRequestException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: data.message,
+  @Get('/document/:document')
+  async getDocument(@Param('document') document: string) {
+    const customer = await this.customerService.getCustomer({
+      document_number: Number(document),
+    });
+    if (customer !== null) {
+      return this.accountService.getAccount({
+        customer_id: customer.id,
       });
     }
-    return {
-      statusCode: HttpStatus.OK,
-      message: data.message,
-    };
   }
 
-  @Put(':id')
-  async update(@Param('id') id: number) {
-    const data = await this.accountService.update(id);
-    if (data.name === 'error') {
-      throw new BadRequestException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: data.message,
-      });
-    }
-    return {
-      statudCode: HttpStatus.OK,
-      message: data.message,
-    };
+  @Put(':account_number')
+  async updateAccount(
+    @Param('account_number') account_number: string,
+    @Body()
+    accountData: {
+      customer_id: number;
+      agency: number;
+      balance: number;
+      account_status: number;
+    },
+  ): Promise<Account> {
+    return this.accountService.updateAccount({
+      where: {
+        account_number: Number(account_number),
+      },
+      data: accountData,
+    });
   }
 }
